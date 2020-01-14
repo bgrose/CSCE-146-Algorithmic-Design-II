634424a5106c39d63b5c9c2eed5bd5585278447d
Bradley <47512745+bgrose@users.noreply.github.com>
2020-01-14 16:23:58 -0500

    Lab00

---
+++ Mine Counter/src/MineCounter.java
@@ -0,0 +1,29 @@
+/*
+ * Bradley Grose
+ */
+import java.util.Random;
+
+public class MineCounter {
+
+	public ar
+	public static void main(String[] args) {
+
+		System.out.println("Creating Empty Board")
+		emptyboard();
+	
+		
+	}
+	
+	
+	public static void emptyboard()
+	{
+		for(int i=0; i<10; i++)
+		{
+			for(int j=0; j<0; j++)
+			{
+				System.out.print("-");
+			}
+		}
+	}
+
+}
