92da7cd9d5afd3e1564e4bea10ebb048d15321fb
Bradley <47512745+bgrose@users.noreply.github.com>
2020-01-21 17:40:30 -0500

    Class 1/21

--- Team Managment System/src/Student.java
+++ Team Managment System/src/Student.java
@@ -7,5 +7,15 @@ public class Student extends Person {
 		super();
 		this.id = 0;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
 	
 }
--- Team Managment System/src/TeamManager.java
+++ Team Managment System/src/TeamManager.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.io.*;
+
 public class TeamManager {
 
 	public static final int SIZE = 30;
@@ -19,6 +22,7 @@ public class TeamManager {
 		if(size <= 0)
 			init(SIZE);
 	}
+	
 	
 	public void add(Person p)
 	{
@@ -53,5 +57,83 @@ public class TeamManager {
 				return i;
 		}
 		return ret;
+	}
+	
+	public void print()
+	{
+		for(Person p : team)
+			if(p != null)
+				System.out.println(p);
+			else
+				break;
+		
+	}
+	/*
+	 * HEADER
+	 * Num People\t<<# of people>>\n
+	 * BODY 
+	 * <<type>>\t<<name>>\n
+	 * <<type>>\t<<name>>\t<<id>>\n
+	 * <<type>>\t<<name>>\t<<id>>\t<<level>>\n
+	 */
+	
+	public static final String DELIM = "\t";
+	public void printToFile(String aFilename)
+	{
+		try
+		{
+			PrintWriter fileWriter = new PrintWriter(new File(aFilename));
+			//Print Header
+			fileWriter.println("Num Perople"+DELIM+team.length);
+			//Print Body
+			for(Person p : team)
+			{
+				if(p == null)
+					break;
+				if(p.getClass().equals("Person:"))
+					fileWriter.println(p.getClass()+DELIM+p.getName());
+				else if(p.getClass().equals("Student"))
+				{
+					Student s =(Student)p;
+					fileWriter.println(s.getClass()+DELIM+s.getName()+DELIM+s.getId());
+				}
+			}
+			fileWriter.close(); //IMPORTANT
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void readFile(String aFileName)
+	{
+		try
+		{
+			Scanner fileScanner = new Scanner(new File(aFileName));
+			String fileLine = fileScanner.nextLine();
+			String[] splitLine = fileLine.split(DELIM);
+			if(splitLine.length != 2)
+			{
+				return;
+			}
+			int size = Integer.parseInt(splitLine[1]);
+			init(size);
+			while(fileScanner.hasNextLine())
+			{
+				fileLine = fileScanner.nextLine();
+				splitLine = fileLine.split(DELIM);
+				if(splitLine[0].equals("Students"))
+				{
+					String name = splitLine[1];
+					int id = Integer.parseInt(splitLine[2]);
+					add(new Student(name,id));
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
