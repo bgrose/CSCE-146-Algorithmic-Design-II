a11e26d71314411f1bbe9b491731289da4c6b981
Bradley <47512745+bgrose@users.noreply.github.com>
2020-01-26 22:43:46 -0500

    Lab 02 Start
    Not complete, merge code from last semester lab

---
+++ GradeCalculator/src/Grader.java
@@ -0,0 +1,26 @@
+/*
+ * BRadley Grose
+ */
+
+import java.io.FileNotFoundException;
+import java.util.*;
+
+
+public class Grader {
+	public static void main(String[] args) throws FileNotFoundException
+	{
+	
+		System.out.println("Welcome to the Grader Program");
+		Scanner keyboard = new Scanner(System.in);
+		while(true) {
+			System.out.println("Enter a file name or \"quit\" to exit");
+			Student stud = new Student();
+			String input = keyboard.nextLine();
+					if(input.contentEquals("quit"))
+						break;
+			stud.readValues(input);
+			System.out.println(stud.toString());
+			
+		}
+	}
+}
