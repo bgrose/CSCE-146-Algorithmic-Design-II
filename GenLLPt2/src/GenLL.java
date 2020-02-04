2948a2673d9ec022dda58cff320fd8c921c5679e
Bradley <47512745+bgrose@users.noreply.github.com>
2020-02-04 17:26:00 -0500

    InClass 2/4

--- GenLLPt2/src/GenLL.java
+++ GenLLPt2/src/GenLL.java
@@ -1,17 +1,114 @@
 /*
- * In class demo
+ * Generic Linked List By Bradley Grose
  */
 
-public class GenLL <T>{
-	
-	private class ListNode
-	{
+import java.util.Iterator;
+
+public class GenLL<T> implements Iterable<T> { // implements Iterable<T> allows for each loop
+
+	private class ListNode {
 		T data;
 		ListNode link;
-		public ListNode(T aData, ListNode aLink)
-		{
+
+		public ListNode(T aData, ListNode aLink) {
 			data = aData;
 			link = aLink;
 		}
+	}
+
+	public class ListIterator implements Iterator<T> {
+		private ListNode iCurr;
+
+		public ListIterator(ListNode head) {
+			iCurr = head;
+		}
+
+		public boolean hasNext() {
+			return iCurr != null;
+		}
+
+		public T next() {
+			T ret = iCurr.data;
+			iCurr = iCurr.link;
+			return ret;
+		}
+	}
+
+	private ListNode head; // First Element
+	private ListNode curr; // Current Data Element
+	private ListNode prev; // One Node behind current
+	// private ListNode tail; //Always Last Element
+
+	public GenLL() {
+		head = curr = prev = null;
+	}
+
+	// Use to do for each loops
+	public Iterator<T> iterator() {
+		return new ListIterator(head);
+	}
+
+	public void add(T aData) {
+		ListNode newNode = new ListNode(aData, null);
+		if (head == null) {
+			head = curr = newNode;
+			return;
+		}
+		ListNode temp = head;
+		while (temp.link != null)
+			temp = temp.link;
+		temp.link = newNode;
+	}
+
+	public void removeCurrent() {
+		if (curr != null && prev == null) {
+			head = head.link;
+			curr = head;
+		} else if (curr != null && prev != null) {
+			prev.link = curr.link;
+			curr = curr.link;
+
+		}
+	}
+
+	public void goToNext() {
+		if (curr != null)
+			curr = curr.link;
+	}
+
+	public void resetCurrent() {
+		curr = head;
+	}
+
+	public T getCurrent() {
+		if (curr != null)
+			return curr.data;
+		return null;
+	}
+
+	public void setCurrent(T aData) {
+		if (curr != null)
+			curr.data = aData;
+	}
+
+	public boolean hasMore() {
+		return curr != null;
+	}
+
+	public void print() {
+		ListNode temp = head;
+		while (temp != null) {
+			System.out.println(temp.data);
+			temp = temp.link;
+		}
+	}
+
+	public static void main(String[] args) {
+		GenLL<String> sLL = new GenLL<String>();
+		for (int i = 0; i < 10; i++)
+			sLL.add("String " + i);
+		for (String s : sLL)
+			System.out.println(s);
+
 	}
 }
