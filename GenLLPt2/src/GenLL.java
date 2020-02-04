e8928a28fd2aebdc8da4c8e4d6512f253ad5efb4
Bradley <47512745+bgrose@users.noreply.github.com>
2020-02-04 16:32:35 -0500

    LabReport03

---
+++ GenLLPt2/src/GenLL.java
@@ -0,0 +1,17 @@
+/*
+ * In class demo
+ */
+
+public class GenLL <T>{
+	
+	private class ListNode
+	{
+		T data;
+		ListNode link;
+		public ListNode(T aData, ListNode aLink)
+		{
+			data = aData;
+			link = aLink;
+		}
+	}
+}
