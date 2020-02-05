b158fc9bb3ddbf082cfe64cc622572567cc65648
Bradley <47512745+bgrose@users.noreply.github.com>
2020-02-05 09:53:29 -0500

    Format Rules

--- DoubleLinkedList/src/GenDoubleLinkedList.java
+++ DoubleLinkedList/src/GenDoubleLinkedList.java
@@ -2,154 +2,134 @@
 /*
  * Bradley Grose
  */
-public class GenDoubleLinkedList <T>{
+public class GenDoubleLinkedList<T> {
 
 	private class ListNode {
 		private T data;
 		private ListNode nextLink;
 		private ListNode prevLink;
-		
-		
-		public ListNode() { //Constructor
+
+		public ListNode() { // Constructor
 			data = null;
 			nextLink = null;
 			prevLink = null;
 		}
-		
-		//Paramaterized
-		public ListNode(T aData, ListNode aNextLink, ListNode aPrevLink)
-		{
+
+		// Paramaterized
+		public ListNode(T aData, ListNode aNextLink, ListNode aPrevLink) {
 			data = aData;
 			nextLink = aNextLink;
 			prevLink = aPrevLink;
 		}
 	}
-	
-	//Spot Holders
+
+	// Spot Holders
 	private ListNode head;
 	private ListNode curr;
-	
-	//Constructor of List
-	public GenDoubleLinkedList()
-	{
+
+	// Constructor of List
+	public GenDoubleLinkedList() {
 		head = curr = null;
 	}
-	
-	//Moves forward
+
+	// Moves forward
 	public void goToNext() {
-		if(curr == null || curr.nextLink == null)
+		if (curr == null || curr.nextLink == null)
 			return;
 		curr = curr.nextLink;
 	}
-	
-	//Goes to previous
-	public void goToPrev()
-	{
-		if(curr == null)
+
+	// Goes to previous
+	public void goToPrev() {
+		if (curr == null)
 			return;
 		curr = curr.prevLink;
 	}
-	
-	//Is there another one
-	public boolean moreToIterate()
-	{
-		return curr != null &&
-				curr.nextLink != null;
+
+	// Is there another one
+	public boolean moreToIterate() {
+		return curr != null && curr.nextLink != null;
 	}
-	
-	//Resets counter
-	public void resetCurrent() 
-	{
+
+	// Resets counter
+	public void resetCurrent() {
 		curr = head;
 	}
-	
-	//Sets data type
-	public void setCurrent(T aData)
-	{
-		if(curr != null)
-		{
+
+	// Sets data type
+	public void setCurrent(T aData) {
+		if (curr != null) {
 			curr.data = aData;
 		}
 	}
-	
-	//Get data
-	public T getCurrent()
-	{
-		if(curr == null)
+
+	// Get data
+	public T getCurrent() {
+		if (curr == null)
 			return null;
 		return curr.data;
 	}
-	
-	//Insert Data
-	public void insert(T aData)
-	{
-		ListNode newNode = new ListNode(aData,null, null);
-		if(head == null) //Means List is empty
+
+	// Insert Data
+	public void insert(T aData) {
+		ListNode newNode = new ListNode(aData, null, null);
+		if (head == null) // Means List is empty
 		{
-			head = curr = newNode; //Creates pointer to head of list and adds newNode
+			head = curr = newNode; // Creates pointer to head of list and adds newNode
 			return;
 		}
-		
-		
-		ListNode temp = head; //Temp Pointer
-		while(temp.nextLink != null)
-		{
+
+		ListNode temp = head; // Temp Pointer
+		while (temp.nextLink != null) {
 			temp = temp.nextLink;
-		} 
-		temp.nextLink = newNode; //Creates a Node at the End
+		}
+		temp.nextLink = newNode; // Creates a Node at the End
 		newNode.prevLink = temp;
 	}
-	
-	//Insert After Current
-	public void insertAfterCurrent(T aData)
-	{
-		if(curr == null)
+
+	// Insert After Current
+	public void insertAfterCurrent(T aData) {
+		if (curr == null)
 			return;
 		ListNode newNode = new ListNode(aData, curr.nextLink, null);
 		newNode.nextLink = curr.nextLink;
 		newNode.prevLink = curr;
 		curr.nextLink = newNode;
 	}
-	
-	//Deletes current slot
-	public void deleteCurrent()
-	{
-		if(curr == null)
+
+	// Deletes current slot
+	public void deleteCurrent() {
+		if (curr == null)
 			return;
-		if(curr == head)
-		{
+		if (curr == head) {
 			head = head.nextLink;
 			head.prevLink = null;
 			curr = head;
 			return;
 		}
-		
-		//Continued
+
+		// Continued
 		curr.prevLink.nextLink = curr.nextLink;
-		if(curr.nextLink != null)
-		{
+		if (curr.nextLink != null) {
 			curr.nextLink.prevLink = curr.prevLink;
 		}
 		curr = curr.nextLink;
 	}
-	
-	public void print() //Runs a While loop to print with a linked list
+
+	public void print() // Runs a While loop to print with a linked list
 	{
 		ListNode temp = head;
-		while(temp != null)
-		{
+		while (temp != null) {
 			System.out.println(temp.data);
 			temp = temp.nextLink;
 		}
 	}
-	
-	//Checks to see if the data is contained
-	public boolean contains(T aData)
-	{
+
+	// Checks to see if the data is contained
+	public boolean contains(T aData) {
 		ListNode temp = head;
-		while(temp != null)
-		{
-			if(temp.data.equals(aData))
+		while (temp != null) {
+			if (temp.data.equals(aData))
 				return true;
 			temp = temp.nextLink;
 		}
--- HotPotato/src/GenCircLinkedList.java
+++ HotPotato/src/GenCircLinkedList.java
@@ -1,143 +1,122 @@
 /*
  * Bradley Grosr
  */
-public class GenCircLinkedList <T> {
+public class GenCircLinkedList<T> {
 
-	//Class for ListNode
-	class ListNode{
+	// Class for ListNode
+	class ListNode {
 		T data;
 		ListNode link;
-		
-		public ListNode(T data, ListNode link)
-		{
+
+		public ListNode(T data, ListNode link) {
 			this.data = data;
 			this.link = link;
 		}
 	}
-	
-	//Define Values
+
+	// Define Values
 	private ListNode head, prev, curr, tail;
 	private int size;
 
-	//Make List
-	public GenCircLinkedList()
-	{
+	// Make List
+	public GenCircLinkedList() {
 		head = new ListNode(null, null);
 		curr = head = tail;
 	}
-	
-	//Goes to Next Value
+
+	// Goes to Next Value
 	public void goToNext() {
-	
-		if(curr != null && curr == tail) //Loops if at tail
+
+		if (curr != null && curr == tail) // Loops if at tail
 		{
 			prev = curr;
 			curr = head;
-		}
-		else if(curr != null) //Continues
+		} else if (curr != null) // Continues
 		{
-			prev = curr; 
-			curr = curr.link; 
+			prev = curr;
+			curr = curr.link;
 		}
 	}
-	
-	//Resets to head
-	public void resetCurrent()
-	{
+
+	// Resets to head
+	public void resetCurrent() {
 		curr = head;
 		prev = tail;
 	}
-	
-	public T getDataAtCurrent()
-	{
-		if(curr != null)
+
+	public T getDataAtCurrent() {
+		if (curr != null)
 			return curr.data;
 		return null;
 	}
-	
-	public void setDataAtCurr(T data)
-	{
-		if(curr!=null)
+
+	public void setDataAtCurr(T data) {
+		if (curr != null)
 			curr.data = data;
 	}
-	
-	//Add Player
-	public void add(T data)
-	{
+
+	// Add Player
+	public void add(T data) {
 		ListNode newNode = new ListNode(data, null);
-		
-		if(head == null || head.data == null)
-		{
+
+		if (head == null || head.data == null) {
 			head = curr = tail = newNode;
 			size++;
 			return;
 		}
-		if (head!= tail) 
+		if (head != tail)
 			tail.link = newNode;
 		else
 			head.link = newNode;
-		
+
 		tail = newNode;
 		newNode.link = head;
-		size ++;
+		size++;
 	}
-	
-	//Used to Delete and relink circle
-	public void goToPrev()
-	{
-		if(curr != null && prev != null)
-		{
+
+	// Used to Delete and relink circle
+	public void goToPrev() {
+		if (curr != null && prev != null) {
 			ListNode probe = head;
-			while(probe.link != prev && probe.link != curr)
-			{
+			while (probe.link != prev && probe.link != curr) {
 				probe = probe.link;
 			}
-			
+
 			prev = probe;
 			curr = prev;
 		}
-			
+
 	}
-	
-	//Deletes Current Player
-	public void delete(){
-		if(curr == null)
+
+	// Deletes Current Player
+	public void delete() {
+		if (curr == null)
 			return;
-		if(curr != head && curr != tail)
-		{
+		if (curr != head && curr != tail) {
 			ListNode temp = curr.link;
 			curr.link = null;
 			goToPrev();
 			curr.link = temp;
-		}
-		else if (curr == head)
-		{
+		} else if (curr == head) {
 			ListNode temp = head.link;
 			tail.link = temp;
 			head = curr = temp;
-		}
-		else if (curr == tail)
-		{
+		} else if (curr == tail) {
 			tail.link = null;
 			goToPrev();
 			curr.link = head;
 		}
-		
+
 		size--;
 	}
-	
-	//Getters
+
+	// Getters
 	public ListNode getCurrent() {
 		return curr;
 	}
-	
+
 	public int getSize() {
 		return this.size;
 	}
-	
-	
-	
-	
-	
-	
+
 }
