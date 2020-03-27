66630718a743aef3366e8cdf235dfa55186bfd3a
Bradley <47512745+bgrose@users.noreply.github.com>
2020-03-27 12:01:34 -0400

    Binary Search Tree 2

--- BinarySearchTree/src/BinarySearchTree.java
+++ BinarySearchTree/src/BinarySearchTree.java
@@ -1,31 +1,30 @@
 
-public class BinarySearchTree <T extends Comparable<T>> {
+public class BinarySearchTree<T extends Comparable<T>> {
 
-	private class Node
-	{
+	private class Node {
 		private T data;
-		private Node leftChild; //Less than
-		private Node rightChild; //Greater than
-		public Node(T aData)
-		{
+		private Node leftChild; // Less than
+		private Node rightChild; // Greater than
+
+		public Node(T aData) {
 			data = aData;
 			leftChild = rightChild = null;
 		}
 	}
-	
+
 	private Node root;
-	public BinarySearchTree()
-	{
+
+	public BinarySearchTree() {
 		root = null;
 	}
-	public void add(T aData)
-	{
-		if(root == null)
+
+	public void add(T aData) {
+		if (root == null)
 			root = new Node(aData);
 		else
 			add(root, aData);
 	}
-	
+
 	private Node add(Node aNode, T aData) {
 		if (aNode == null) // Found a Leef
 			aNode = new Node(aData);
@@ -36,36 +35,80 @@ public class BinarySearchTree <T extends Comparable<T>> {
 		return aNode;
 
 	}
-	
-	public void printPreOrder()
-	{
+
+	public void printPreOrder() {
 		printPreOrder(root);
 	}
-	
-	private void printPreOrder(Node aNode)
-	{
-		if(aNode == null)
+
+	private void printPreOrder(Node aNode) {
+		if (aNode == null)
 			return;
-		System.out.println(aNode.data); //Process
-		printPreOrder(aNode.leftChild); //Go Left
-		printPreOrder(aNode.rightChild); //Go Right
-		
+		System.out.println(aNode.data); // Process
+		printPreOrder(aNode.leftChild); // Go Left
+		printPreOrder(aNode.rightChild); // Go Right
+
 	}
-	
-	public void printInOrder()
-	{
+
+	public void printInOrder() {
 		printInOrder(root);
 	}
-	
-	public void printInOrder(Node aNode)
-	{
-		if(aNode == null)
+
+	public void printInOrder(Node aNode) {
+		if (aNode == null)
 			return;
 		printInOrder(aNode.leftChild);
 		System.out.println(aNode.data);
 		printInOrder(aNode.rightChild);
 	}
-	
-	
-	
+
+	public boolean search(T aData) {
+		return recursiveSearch(root, aData);
+	}
+
+	public boolean recursiveSearch(Node aNode, T aData) {
+		if (aNode == null)
+			return false;
+		if (aNode.data.compareTo(aData) == 0)
+			return true;
+		else if (aData.compareTo(aNode.data) < 0)
+			return recursiveSearch(aNode.leftChild, aData);
+		else
+			return recursiveSearch(aNode.rightChild, aData);
+	}
+
+	public void remove(T aData) {
+		root = remove(root, aData);
+	}
+
+	private Node remove(Node aNode, T aData) {
+		if (aNode == null)
+			return null;
+		if (aData.compareTo(aNode.data) < 0)
+			aNode.leftChild = remove(aNode.leftChild, aData);
+		else if (aData.compareTo(aNode.data) > 0)
+			aNode.rightChild = remove(aNode.rightChild, aData);
+		else {
+			// Case 1
+			if (aNode.rightChild == null)
+				return aNode.leftChild;
+			// Case 2
+			if (aNode.leftChild == null)
+				return aNode.rightChild;
+			// Case 3
+			Node temp = findMinInTree(aNode.rightChild);
+			aNode.data = temp.data;
+			aNode.rightChild = remove(aNode.rightChild, temp.data);
+		}
+		return aNode;
+	}
+
+	private Node findMinInTree(Node aNode) {
+		if (aNode == null)
+			return null;
+		if (aNode.leftChild == null)
+			return aNode;
+		else
+			return findMinInTree(aNode.leftChild);
+	}
+
 }
